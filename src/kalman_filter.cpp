e3274bb55c4d38eda6b9b4dea2687d7a5c9b0726
yang song <ysong.sc@gmail.com>
2018-12-27 21:05:01 +0000

    solution not formatted

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,12 +1,12 @@
 #include "kalman_filter.h"
+#include <iostream>
+#include <math.h>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-/* 
- * Please note that the Eigen library does not initialize 
- *   VectorXd or MatrixXd objects with zeros upon creation.
- */
+// Please note that the Eigen library does not initialize
+// VectorXd or MatrixXd objects with zeros upon creation.
 
 KalmanFilter::KalmanFilter() {}
 
@@ -14,28 +14,69 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+    /**
+       TODO:
+       * predict the state
+       */
+    x_ = F_ * x_;
+    P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+    /**
+       TODO:
+       * update the state by using Kalman Filter equations
+       */
+    VectorXd y_ = z  - H_ * x_;
+    MatrixXd S_ = H_ * P_ * H_.transpose() + R_;
+    MatrixXd K_ = P_ * H_.transpose() * S_.inverse();
+    MatrixXd I = MatrixXd::Identity(4, 4);
+
+    x_ = x_ + K_ * y_;
+    P_ = (I - K_* H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+    /**
+       TODO:
+       * update the state by using Extended Kalman Filter equations
+       */
+    // measurement function h(x') that maps the predicted state x' into the measurement space
+    float px = x_[0], py = x_[1], vx = x_[2], vy = x_[3];
+    //std::cout<<"px = "<<px<<" py = "<<py<<" vx = "<<vx <<" vy = "<<vy <<std::endl;
+    VectorXd hx(3);
+    hx[0] = sqrt(px * px + py * py);
+    hx[1] = atan2(py, px);
+
+    if (fabs(hx[0]) < 0.0001) {
+        hx[2] = (px * vx + py * vy)/0.0001;
+    }else{
+        hx[2] = (px * vx + py * vy)/hx[0];
+    }
+    // update Measurement prediction: z_ = h(x') + noise_matrix
+    VectorXd y_ = z - hx;
+    // restrict the orientation of car in [-PI, PI],
+    if (y_[1] > M_PI) {
+        y_[1] -= 2 * M_PI;
+    }else if (y_[1] < -M_PI){
+        y_[1] += 2 * M_PI;
+    }
+    //std::cout<<"y_="<<y_<<std::endl;
+    MatrixXd S_ = H_ * P_ * H_.transpose() + R_;
+    std::cout<<"S_="<<S_<<std::endl;
+    MatrixXd K_ = P_ * H_.transpose() * S_.inverse();
+    //std::cout<<"K_="<<K_<<std::endl;
+    MatrixXd I = MatrixXd::Identity(4, 4);
+    // new estimation
+    x_ = x_ + K_ * y_;
+    P_ = (I - K_ * H_) * P_;
 }
